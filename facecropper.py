d08d72498c3b31b048b1332ae1729062f423e9c7
Cloud Shao <cshao@riotgames.com>
2018-02-25 14:29:09 +0800

    Crop list of people and output profiles.json

--- facecropper.py
+++ facecropper.py
@@ -1,7 +1,8 @@
 from PIL import Image, ImageDraw, ImageOps, ImageEnhance
 import face_recognition
+import os
 
-def crop_image_to_face(filename):
+def crop_image_to_face(filename, output_dir):
 
     # Load the jpg file into a numpy array
     image = face_recognition.load_image_file(filename)
@@ -11,61 +12,68 @@ def crop_image_to_face(filename):
 
     print("I found {} face(s) in {}.".format(len(face_landmarks_list), filename))
 
-    for face_landmarks in face_landmarks_list:
+    face_landmarks = face_landmarks_list[0]
 
-        # Print the location of each facial feature in this image
-        facial_features = [
-            'chin',
-            'left_eyebrow',
-            'right_eyebrow',
-            'nose_bridge',
-            'nose_tip',
-            'left_eye',
-            'right_eye',
-            'top_lip',
-            'bottom_lip'
-        ]
+    # Print the location of each facial feature in this image
+    facial_features = [
+        'chin',
+        'left_eyebrow',
+        'right_eyebrow',
+        'nose_bridge',
+        'nose_tip',
+        'left_eye',
+        'right_eye',
+        'top_lip',
+        'bottom_lip'
+    ]
 
-        for facial_feature in facial_features:
-            print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+    for facial_feature in facial_features:
+        print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
 
-        chin = face_landmarks['chin']
-        left = 9999
-        right = 0
-        bottom = 0
-        middle = 9999
-        for x,y in chin:
-            middle = y if x < left else middle # use leftmost as vertical center
-            left = x if x < left else left
-            right = x if x > right else right
-            bottom = y if y > bottom else bottom
+    chin = face_landmarks['chin']
+    left = 9999
+    right = 0
+    bottom = 0
+    middle = 9999
+    for x,y in chin:
+        middle = y if x < left else middle # use leftmost as vertical center
+        left = x if x < left else left
+        right = x if x > right else right
+        bottom = y if y > bottom else bottom
 
-        left_eye = face_landmarks['left_eye']
-        top = bottom - 1.618*(bottom - left_eye[0][1]) # golden ratio
-        print("Left: {} Right: {} Bottom: {} Top: {}".format(left, right, bottom, top))
+    left_eye = face_landmarks['left_eye']
+    top = bottom - 1.618*(bottom - left_eye[0][1]) # golden ratio
+    print("Left: {} Right: {} Bottom: {} Top: {}".format(left, right, bottom, top))
 
-        # Let's trace out each facial feature in the image with a line!
-        pil_image = Image.fromarray(image)
+    # Let's trace out each facial feature in the image with a line!
+    pil_image = Image.fromarray(image)
 
-        # Convert to greyscale
-        #pil_image = ImageOps.grayscale(pil_image)
-        #pil_image = pil_image.convert("L")
-        enhancer = ImageEnhance.Color(pil_image)
-        pil_image = enhancer.enhance(0.0)
+    # Convert to greyscale
+    #pil_image = ImageOps.grayscale(pil_image)
+    #pil_image = pil_image.convert("L")
+    enhancer = ImageEnhance.Color(pil_image)
+    pil_image = enhancer.enhance(0.0)
 
-        # Crop
-        pil_image = pil_image.crop((left, top, right, bottom))
+    # Crop
+    pil_image = pil_image.crop((left, top, right, bottom))
 
-        bigsize = (pil_image.size[0]*3, pil_image.size[1]*3)
-        mask = Image.new('L', bigsize, 0)
-        d = ImageDraw.Draw(mask)
-        #d.ellipse([left*3, top*3, right*3, bottom*3], fill=255)
-        d.ellipse((0,0)+bigsize, fill=255)
-        mask = mask.resize(pil_image.size, Image.ANTIALIAS)
-        pil_image.putalpha(mask)
+    bigsize = (pil_image.size[0]*3, pil_image.size[1]*3)
+    mask = Image.new('L', bigsize, 0)
+    d = ImageDraw.Draw(mask)
+    #d.ellipse([left*3, top*3, right*3, bottom*3], fill=255)
+    d.ellipse((0,0)+bigsize, fill=255)
+    mask = mask.resize(pil_image.size, Image.ANTIALIAS)
+    pil_image.putalpha(mask)
 
-        pil_image.show()
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-        #for facial_feature in facial_features:
-        #    d.line(face_landmarks[facial_feature], width=5)
-        #pil_image.show()
+    basename = os.path.basename(filename)
+    savename = "faces/" + basename.split(".")[0] + ".png"
+    pil_image.save(output_dir + "/" + savename)
+
+    return savename
+
+    #for facial_feature in facial_features:
+    #    d.line(face_landmarks[facial_feature], width=5)
+    #pil_image.show()
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 import facecropper
+import json
 
 files = os.listdir("./res")
 
+files = [f for f in files if f.endswith(('.jpg', '.png', '.gif'))]
 print(files)
 
-for f in files:
-    facecropper.crop_image_to_face("res/" + f)
+people = [
+    {'id': 'patrickstewart', 'name': 'Patrick Stewart'},
+    {'id': 'britneyspears', 'name': 'Britney Spears'},
+    {'id': 'leehyori', 'name': 'Lee Hyori'},
+    ]
+
+for p in people:
+    p['images'] = []
+    for f in [f for f in files if f.startswith(p['id'])]:
+        saved = facecropper.crop_image_to_face("res/" + f, "output")
+        p['images'].append(saved)
+
+jsonString = json.dumps(people)
+jsonFile = open('output/profiles.json', 'w')
+jsonFile.write(jsonString)
+jsonFile.close()
