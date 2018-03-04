25fa673dd2cb69795d7b04eca1992002006260b9
Cloud Shao <cshao@riotgames.com>
2018-03-04 14:20:25 +0800

    Card flip animation for card->answer transition

--- mobile/answercard.js
+++ mobile/answercard.js
@@ -1,33 +1,56 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Animated, Easing, StyleSheet, Text, View, Button, Image } from 'react-native';
 import { S3_URL } from './App.js';
 
 export default class AnswerCard extends Component {
+  state = {
+    spinValue: new Animated.Value(0),
+  }
+
   constructor(props) {
     super(props);
   }
+
+  componentDidMount(){
+    Animated.timing(this.state.spinValue, {
+        toValue: 100,
+        duration: 300,
+        easing: Easing.out(Easing.cubic),
+      }).start();
+  }
+
+  spinInterp = this.state.spinValue.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['-90deg', '0deg']
+  });
 
   render() {
 
     const imageIdx = this.props.nextInterval % this.props.images.length;
 
+    let animStyle = {
+      transform: [{rotateY: this.spinInterp}]
+    };
+
     return (
-      <View style={styles.container}>
-        <Image source={{uri: S3_URL + '/' + this.props.images[imageIdx]}}
-               style={styles.portrait} />
-        <Text>ID: {this.props.id}</Text>
-        <Text>Name: {this.props.name}</Text>
-        <Text>Images: {this.props.images.length} {this.props.images}</Text>
-        <Text>Due date: {this.props.dueDate}</Text>
-        <Text>Current date: {Date.now()}</Text>
-        <Text>Next interval: {this.props.nextInterval}</Text>
-        <Button
-          onPress={() => {this.props.controller.next(true);}}
-          title="Right" />
-        <Button
-          onPress={() => {this.props.controller.next(false);}}
-          title="Wrong" />
-      </View>
+      <Animated.View style={[styles.container, animStyle]}>
+        <View style={styles.container}>
+          <Image source={{uri: S3_URL + '/' + this.props.images[imageIdx]}}
+                 style={styles.portrait} />
+          <Text>ID: {this.props.id}</Text>
+          <Text>Name: {this.props.displayname}</Text>
+          <Text>Images: {this.props.images.length} {this.props.images}</Text>
+          <Text>Due date: {this.props.dueDate}</Text>
+          <Text>Current date: {Date.now()}</Text>
+          <Text>Next interval: {this.props.nextInterval}</Text>
+          <Button
+            onPress={() => {this.props.controller.next(true);}}
+            title="Right" />
+          <Button
+            onPress={() => {this.props.controller.next(false);}}
+            title="Wrong" />
+        </View>
+      </Animated.View>
     );
   }
 }
--- mobile/card.js
+++ mobile/card.js
@@ -1,29 +1,52 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Animated, Easing, StyleSheet, Text, View, Button, Image } from 'react-native';
 import { S3_URL } from './App.js';
 
 export default class Card extends Component {
+  state = {
+    spinValue: new Animated.Value(0),
+  }
+
   constructor(props) {
     super(props);
   }
+
+  spin(callback) {
+    Animated.timing(this.state.spinValue, {
+        toValue: 100,
+        duration: 300,
+        easing: Easing.in(Easing.cubic),
+      }).start(callback);
+  }
+
+  spinInterp = this.state.spinValue.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0deg', '90deg']
+  });
 
   render() {
 
     const imageIdx = this.props.nextInterval % this.props.images.length;
 
+    let animStyle = {
+      transform: [{rotateY: this.spinInterp}]
+    };
+
     return (
-      <View style={styles.container}>
-        <Image source={{uri: S3_URL + '/' + this.props.images[imageIdx]}}
-               style={styles.portrait} />
-        <Text>ID: {this.props.id}</Text>
-        <Text>Images: {this.props.images.length} {this.props.images}</Text>
-        <Text>Due date: {this.props.dueDate.toLocaleString()}</Text>
-        <Text>Current date: {new Date().toLocaleString()}</Text>
-        <Text>Next interval: {this.props.nextInterval}</Text>
-        <Button
-          onPress={() => {this.props.controller.flipCard(true);}}
-          title="Show Answer" />
-      </View>
+      <Animated.View style={[styles.container, animStyle]}>
+        <View style={styles.container}>
+          <Image source={{uri: S3_URL + '/' + this.props.images[imageIdx]}}
+                 style={styles.portrait} />
+          <Text>ID: {this.props.id}</Text>
+          <Text>Images: {this.props.images.length} {this.props.images}</Text>
+          <Text>Due date: {this.props.dueDate.toLocaleString()}</Text>
+          <Text>Current date: {new Date().toLocaleString()}</Text>
+          <Text>Next interval: {this.props.nextInterval}</Text>
+          <Button
+            onPress={() => {this.spin(() => {this.props.controller.flipCard(true);})}}
+            title="Show Answer" />
+        </View>
+      </Animated.View>
     );
   }
 }
